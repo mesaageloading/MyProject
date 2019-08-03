7850aa4370624a02d3164d038f7f7a6015a34e17
lsl <584838516@qq.com>
2019-08-03 15:03:19 +0800

    新增项目百度网盘

--- 项目3：企业网站设计稿-大宗商品交易/bulk_stock_project/js/index.js
+++ 项目3：企业网站设计稿-大宗商品交易/bulk_stock_project/js/index.js
@@ -1,20 +1,29 @@
-function changeBtn (){
-    let newPanel = document.getElementById("news-panel")
-    let oBtn = newPanel.getElementsByTagName("button")
-    let nowIndex = 0;
+function changeBtn() {
+    this.bodyDay = document.getElementById("body-day")
+    this.bodyMoney = document.getElementById("body-money")
+    this.btnDay = document.getElementById("btn-day")
+    this.btnMoney = document.getElementById("btn-money")
 }
-changeBtn.prototype.init = function(){
-    let that = this
-    for(let i in that.oBtn){
-        this.oBtn[i].onmouseover = function(){
-            that.oBtn[nowIndex].style.backgroundColor = "transparent";
-            that.style.backgroundColor = "white"
-            that.nowIndex = i;
-        }
-    }
 
+changeBtn.prototype.init = function(){
+    let that =this
+    console.log(that.btnDay)
+    this.btnDay.onmousedown = function () {
+        that.btnMoney.style.backgroundColor = "transparent"
+        that.bodyMoney.style.display = "none"
+        that.bodyDay.style.display = "block"
+        that.btnDay.style.backgroundColor = "white"
+    };
+
+    this.btnMoney.onmousedown = function (){
+        that.btnDay.style.backgroundColor = "transparent"
+        that.bodyDay.style.display = "none"
+        that.bodyMoney.style.display = "block"
+        that.btnMoney.style.backgroundColor = "white"
+
+    }
 }
-changeBtn.prototype.mouse
+
 
 let d1 = new changeBtn();
 d1.init();
